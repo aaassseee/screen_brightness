9b06b45e68ced5bfca1b17c8691502a55e4a073a
Jack Liu <ywp033319@gmail.com>
2021-12-28 14:22:38 +0800

    platform interface support has changed

--- screen_brightness_platform_interface/lib/constant/method_name.dart
+++ screen_brightness_platform_interface/lib/constant/method_name.dart
@@ -2,3 +2,5 @@ const methodNameGetSystemScreenBrightness = 'getSystemScreenBrightness';
 const methodNameGetScreenBrightness = 'getScreenBrightness';
 const methodNameSetScreenBrightness = 'setScreenBrightness';
 const methodNameResetScreenBrightness = 'resetScreenBrightness';
+
+const methodNameHasChanged = 'hasChanged';
