0462e61c58b640299ac55d275a659d051bc0aded
Jack Liu <ywp033319@gmail.com>
2022-05-12 19:21:30 +0800

    added missing idea files

---
+++ screen_brightness_macos/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import screen_brightness_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ScreenBrightnessMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenBrightnessMacosPlugin"))
+}
