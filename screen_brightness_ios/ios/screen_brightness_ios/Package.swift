a32bbc1fa7a00ce1969f96583ced4731328d9a62
Jack Liu <ywp033319@gmail.com>
2025-05-06 22:22:48 +0800

    feat: support spm on macos plugin

--- screen_brightness_ios/ios/screen_brightness_ios/Package.swift
+++ screen_brightness_ios/ios/screen_brightness_ios/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "screen_brightness_ios",
     platforms: [
-        .iOS("12.0"),
+        .iOS("9.0"),
     ],
     products: [
         .library(name: "screen_brightness_ios", targets: ["screen_brightness_ios"])
